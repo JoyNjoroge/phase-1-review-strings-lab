db481ae2a94ed0ca20c42239631b99dcdd272047
Joy Njoroge <njorogejoy78@gmail.com>
2025-03-18 10:24:50 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "joy";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`
+console.log(welcomeMessage);
+console.log(excitedWelcomeMessage);
+console.log(shortGreeting);
